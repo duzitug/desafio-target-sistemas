bab013a7960939a828c31b7991fe1b0df653e546
merciof <merciofilho@gmail.com>
2023-03-02 11:11:15 -0300

    add questao5

---
+++ questao5.js
@@ -0,0 +1,8 @@
+let inputString = "String para reverter";
+let reversedString = "";
+
+for (let i = inputString.length - 1; i >= 0; i--) {
+  reversedString += inputString[i];
+}
+
+console.log(reversedString);
