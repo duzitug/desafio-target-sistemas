b94ca063f99e7ac39f83c3d0344f5ab4dc145888
merciof <merciofilho@gmail.com>
2023-03-02 10:47:20 -0300

    add questao2

---
+++ questao2.js
@@ -0,0 +1,19 @@
+function fibonacci(n) {
+  if (n < 1) return 0;
+  else if (n === 1 || n === 2) return 1;
+  else return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+function isFibonacci(n) {
+  for (let i = 1; i <= n; i++) {
+    if (fibonacci(i) === n) return true;
+  }
+  return false;
+}
+
+const numeroDigitado = parseInt(prompt("Digite um número: "));
+const isFibonacci = isFibonacci(numeroDigitado);
+
+if (isFibonacci)
+  console.log(`${numeroDigitado} pertence à sequência de Fibonacci.`);
+else console.log(`${numeroDigitado} não pertence à sequência de Fibonacci.`);
